b13f62c2a5f3476ef1fcb1629ec29fbbdd69b6ea
albert <albertbergagatius@gmail.com>
2015-12-09 20:46:47 +0100

    Pseudocodi heuristica

--- alphaBeta.cpp
+++ alphaBeta.cpp
@@ -18,6 +18,18 @@ int Utility(State state){
 	}
 }
 
+list< pair<int, int> > visiteds;
+
 int BFS(State state){
+	queue< queue < pair<int, int> > > bfsQueue;
+	queue< pair<int, int> > result;
+
+	visiteds.push_front( make_pair(i->getX(), i->getY()));
+	buidar i afegir primeres cues de camins a bfsQueue comprovant si ja hem trobat el pacman;
+	while(true){
+		si la cua nomes te un element ja hem trobat cap on es moura el fantasma pero seguirem desenvolupant per marcar el cami per on pasem.
+		desenvolupar següent element de la cua comprobant si hem trobat el pacman i si podem continuar per akell cami (visiteds);
+		quan trobem el pacman o no podem continuar pq esta tot marcat, sortim del bucle
+	}
+	marcar posicions de result i determinar cap on anira el fantasma
 	
-}
